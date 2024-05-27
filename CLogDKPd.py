b9e869b423b3dedd16b567672c1e66a60a042848
Faustino Sachimuco <57041760+FaustinoSachimuco@users.noreply.github.com>
2024-05-28 00:17:39 +0100

    Add files via upload
    Actualizacao_28_05_2024_00_17_Min

--- CLogDKPd.py
+++ CLogDKPd.py
@@ -4,34 +4,53 @@ import math
 import matplotlib.pyplot as plt
 import random
 
+#def sigmoid(z):
+    #return 1 / (1 + np.exp(-z))
+    
+
 def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
-def sinal(z):
+    return 1 / (1 + np.exp(-np.clip(z, -500, 500)))
+
+def threshold(z):
     return 1 if z > 0.5 else 0
 def batche(lista, n):
     # Seleciona aleatoriamente n índices únicos da lista
     indices_aleatorios = random.sample(range(len(lista)), n)
     return indices_aleatorios
 
+
+
 def calcular_erro(alpha, X, Y, A):
     erro = 0
     N = len(X)
+    epsilon = 1e-12
     for n in range(N):
-        p_chapeu = sigmoid(sum(alpha * (A[n]) ))
-        if(1 - p_chapeu)<np.e**-12:
-            erro -= np.e**-12
-        else:
-            erro -= (Y[n] * np.log(p_chapeu) + (1 - Y[n]) * np.log(1 - p_chapeu))
-    erro = erro/N
-    return erro if erro > np.e**-12 else np.e**-12
+        p_chapeu = sigmoid(np.sum(alpha * A[n]))
+        erro -= (Y[n] * np.log(p_chapeu + epsilon) + (1 - Y[n]) * np.log(1 - p_chapeu + epsilon))
+    erro = max(erro / N, epsilon)
+    return erro
+
+
+
+#def calcular_erro(alpha, X, Y, A):
+#   erro = 0
+#    N = len(X)
+#    for n in range(N):
+#        p_chapeu = sigmoid(sum(alpha * (A[n]) ))
+#        if(1 - p_chapeu)<np.e**-12:
+#            erro -= np.e**-12
+#        else:
+#            erro -= (Y[n] * np.log(p_chapeu) + (1 - Y[n]) * np.log(1 - p_chapeu))
+#    erro = erro/N
+#    return erro if erro > np.e**-12 else np.e**-12
 
 def CLogDKPd_MGmB(X, Y, alpha, eta,epochs,d,batch_size):
-    t=0
+    t=20
     E=[]
     N=len(X)
     A=np.dot(X,X.T)
     A=A**d
-    
+    count = 0
     for epoca in range(epochs):
         S=[]
         ordem =batche(X, batch_size)
@@ -40,8 +59,11 @@ def CLogDKPd_MGmB(X, Y, alpha, eta,epochs,d,batch_size):
             S.append((p_chapeu-Y[i]) *(A[i]))
         S=np.array(np.sum(S,axis=0))
         alpha = alpha - eta * S
-        E.append(calcular_erro(alpha, X, Y, A))
-        print(alpha)
+        if t ==20:
+            E.append(calcular_erro(alpha, X, Y, A))
+            t = 0
+        t +=1
+        
     return alpha, E
 def CLogDKPd_MGE_Ordenado(X, Y, alpha, eta,epochs,d):
     t=0
